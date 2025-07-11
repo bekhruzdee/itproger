d56c4cd90b94275328cbef3a74c27bd804f9aee5
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-07-11 12:20:45 +0500

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
+<template>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+
+  <button type="button" @click="userData()">Send</button>
+</template>
+
 <script>
+export default {
+  data() {
+    return {
+      info: "Title!",
+      someInfo: "Anons of message",
+    };
+  },
+  methods: {
+    userData() {
+      this.someInfo = 'Some new'
+    }
+  }
+};
 </script>
 
-<template>
-  <h1> Hello </h1>
-</template>
 <style scoped>
-h1 {
-  color: red;
-  
+h3 {
+  font-weight: lighter;
+}
+p {
+  color: rgb(204, 28, 28);
 }
 </style>
